1090d63a4f26f6b905fd0537f88b555f0b40ea99
gmdinaradze <givi@nebuladapps.com>
2023-05-02 19:04:11 +0400

    add p and d

---
+++ packageAnddelivery/src/main/java/com/flatrock/packageanddelivery/PackageAnddeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.flatrock.packageanddelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackageAnddeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackageAnddeliveryApplication.class, args);
+	}
+
+}
---
+++ packageAnddelivery/src/test/java/com/flatrock/packageanddelivery/PackageAnddeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flatrock.packageanddelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackageAnddeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
